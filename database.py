cd332e5f1a9523168be8ab334456c99c2ce865ef
DongHyeok-Bae <dhbae07@naver.com>
2025-06-21 01:35:11 +0900

    동아리음 첫 커밋

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# 1. 데이터베이스 파일 경로 설정 (SQLite 사용)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./dongari.db"
+
+# 2. 데이터베이스 엔진 생성
+# connect_args는 SQLite를 사용할 때만 필요한 설정입니다. (쓰레드 관련)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# 3. 데이터베이스와 통신을 위한 세션(Session) 클래스 생성
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 4. 모델 클래스들이 상속받을 Base 클래스 생성
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base # 방금 만든 database.py에서 Base를 가져옵니다.
+
+# 'groups'라는 이름의 테이블에 매핑될 Group 클래스
+class Group(Base):
+    __tablename__ = "groups"
+
+    # 테이블의 컬럼(속성)들을 정의합니다.
+    id = Column(Integer, primary_key=True, index=True) # 자동 생성될 고유 ID
+    name = Column(String, unique=True, index=True) # 동아리 이름 
+    group_type = Column(String) # 동아리 유형 
+    topic = Column(String)      # 동아리 주제 
+    description = Column(String) # 동아리 설명 
+    password = Column(String(4)) # 4자리 비밀번호
